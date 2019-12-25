dda685874d2e00d57856667fcce67eca701ed824
leandro <leandrogabrielguzman@gmail.com>
2019-12-25 20:49:36 -0300

    mer

--- src/main/java/com/lgg/nticxs/web/controller/FirebaseController.java
+++ src/main/java/com/lgg/nticxs/web/controller/FirebaseController.java
@@ -2,17 +2,22 @@ package com.lgg.nticxs.web.controller;
 
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FirebaseController {
 	
-	@RequestMapping(value = "home/enviartoken/{token}")
-	public void actualizarToken(@PathVariable String token) {
+	@GetMapping(value = "home/enviartoken/{token}")
+	@ResponseBody
+	public String actualizarToken(@PathVariable String token) {
 		System.out.println("este elemento se envio: "+token);
 		//JSONObject message = new JSONObject(jsonString);
 		//System.out.println(message.toString());
+		return "exitoso";
+		
 	}
 
 }
