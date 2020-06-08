9750617eda930ac5565d6e084372704585748263
StevenFernandez98 <steven.fernandez@movasim.com>
2020-06-08 20:31:00 -0300

    mf

--- src/main/java/com/lgg/nticxs/web/AppInitializer.java
+++ src/main/java/com/lgg/nticxs/web/AppInitializer.java
@@ -1,7 +1,10 @@
 package com.lgg.nticxs.web;
 
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +13,10 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.Ssl;
 
 import com.lgg.nticxs.web.DAO.UserDAO;
@@ -35,21 +39,38 @@ public class AppInitializer extends SpringBootServletInitializer{
 		SpringApplication.run(AppInitializer.class, args);
 	}
 	
-//	@Bean
-	private Connector redirectConnector() {
-		  Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		  connector.setScheme("http");
-		  connector.setPort(80);
-		  connector.setSecure(false);
-		  connector.setRedirectPort(443);
-		  return connector;
-		}
 
 	@Bean
 	public UserDAO userDao() {
 		return new UserDAO();
 	}
 	
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(
+                TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 	
 	@Bean
 	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> setConfiguration() {
